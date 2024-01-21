c43d36972c94ad1ec3b1d0eadeb7a98d721f1481
ZeroIDdev <115116337+ZeroIDdev@users.noreply.github.com>
2024-01-21 14:43:28 +0800

    Update index.js

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -3,16 +3,19 @@ import * as controllers from "../controllers/index.js";
 
 const router = express.Router();
 
-router.get("/home", controllers.getHome);
-router.get("/ongoing", controllers.getOnGoing);
-router.get("/completed", controllers.getCompleted);
-router.get("/anime", controllers.getAnime);
-router.get("/movie", controllers.getMovie);
-router.get("/genre", controllers.getGenre);
-router.get("/search", controllers.getSearch);
-router.get("/anime/:slug", controllers.getAnimeDetails);
-router.get("/genre/:slug", controllers.getGenreBySlug);
-router.get("/anime/:slug/:episode", controllers.getStreamingAnime);
-router.get("/movie/:slug", controllers.getStreamingMovie);
+router.get("/home", (req,res)=>{
+  console.log("hello word")
+  res.send('anjay mabar')
+};
+// router.get("/ongoing", controllers.getOnGoing);
+// router.get("/completed", controllers.getCompleted);
+// router.get("/anime", controllers.getAnime);
+// router.get("/movie", controllers.getMovie);
+// router.get("/genre", controllers.getGenre);
+// router.get("/search", controllers.getSearch);
+// router.get("/anime/:slug", controllers.getAnimeDetails);
+// router.get("/genre/:slug", controllers.getGenreBySlug);
+// router.get("/anime/:slug/:episode", controllers.getStreamingAnime);
+// router.get("/movie/:slug", controllers.getStreamingMovie);
 
 export default router;
